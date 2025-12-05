78e8d46afb88728b7149dda914765135af7fca65
Carlos Diaz <carlosdiazvasquez0406@gmail.com>
2025-12-04 22:19:57 -0600

    feature: add Brevo mail transport support

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,9 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Mail; // <--- AGREGA ESTO
+use Symfony\Component\Mailer\Bridge\Brevo\Transport\BrevoTransportFactory; // <--- AGREGA ESTO
+use Symfony\Component\Mailer\Transport\Dsn; // <--- AGREGA ESTO
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,5 +27,11 @@ class AppServiceProvider extends ServiceProvider
         if ($this->app->environment('production')) {
             URL::forceScheme('https');
         }
+        // Registrar el transportador de Brevo
+        Mail::extend('brevo', function (array $config = []) {
+            return (new BrevoTransportFactory)->create(
+                Dsn::fromString('brevo+api://' . $config['key'] . '@default')
+            );
+        });
     }
 }
