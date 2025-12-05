c61b4a0f6cb2e686ae0af5a4fe92c01f0102a1be
Carlos Diaz <carlosdiazvasquez0406@gmail.com>
2025-12-04 20:54:44 -0600

    fix mail configuration to support self-signed certificates and enforce IPv4

--- config/mail.php
+++ config/mail.php
@@ -48,6 +48,16 @@ return [
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
             'timeout' => null,
             'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url((string) env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
+            'stream' => [
+                'ssl' => [
+                   'allow_self_signed' => true,
+                   'verify_peer' => false,
+                   'verify_peer_name' => false,
+                ],
+                'socket' => [
+                    'bindto' => '0.0.0.0:0', // Esto obliga a usar IPv4
+                ],
+            ],
         ],
 
         'ses' => [
